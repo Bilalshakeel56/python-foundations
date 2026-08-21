225c288a4b7f1f32d37aa3a47d704df28ba4e70a
Bilalshakeel56 <bilalshakeelahmed29@gmail.com>
2026-08-21 16:57:43 +0500

    Add Python data types example

---
+++ 01_basics/02_data_types.py
@@ -0,0 +1,19 @@
+# Python Foundations
+# 02 - Data Types
+
+name = "Bilal"       # String
+age = 20             # Integer
+height = 5.9         # Float
+is_student = True    # Boolean
+
+print("Name:", name)
+print("Type:", type(name))
+
+print("Age:", age)
+print("Type:", type(age))
+
+print("Height:", height)
+print("Type:", type(height))
+
+print("Student:", is_student)
+print("Type:", type(is_student))
