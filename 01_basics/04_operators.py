e768ce94bf08c9502c2e001a88f1f62c6b69ddac
Bilalshakeel56 <bilalshakeelahmed29@gmail.com>
2026-08-21 17:07:33 +0500

    Add Python operators example

---
+++ 01_basics/04_operators.py
@@ -0,0 +1,22 @@
+# Python Foundations
+# 04 - Operators
+
+a = 10
+b = 3
+
+# Arithmetic Operators
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Exponent:", a ** b)
+
+# Comparison Operators
+print("Equal:", a == b)
+print("Not Equal:", a != b)
+print("Greater Than:", a > b)
+print("Less Than:", a < b)
+print("Greater or Equal:", a >= b)
+print("Less or Equal:", a <= b)
