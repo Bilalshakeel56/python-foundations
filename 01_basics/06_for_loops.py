2819930f5f121325c8bd0c2dac9ff1c6fab3611a
Bilalshakeel56 <bilalshakeelahmed29@gmail.com>
2026-08-21 17:15:35 +0500

    Add for loop examples

---
+++ 01_basics/06_for_loops.py
@@ -0,0 +1,12 @@
+# Python Foundations
+# 06 - For Loops
+
+# Print numbers from 1 to 5
+for number in range(1, 6):
+    print(number)
+
+# Print the multiplication table
+number = int(input("Enter a number: "))
+
+for i in range(1, 11):
+    print(number, "x", i, "=", number * i)
