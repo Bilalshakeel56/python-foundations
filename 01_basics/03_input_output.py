34b2538dd486b9de1fe64156bb76d2d0e205f0fd
Bilalshakeel56 <bilalshakeelahmed29@gmail.com>
2026-08-21 17:01:32 +0500

    Add input and output example

---
+++ 01_basics/03_input_output.py
@@ -0,0 +1,8 @@
+# Python Foundations
+# 03 - Input and Output
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Hello,", name)
+print("You are", age, "years old.")
